6f6ad05749c9ce03a40967accc6f9b6877b40282
ollybowman <olly@rocksoliddesigns.co.uk>
2026-04-03 19:19:19 +0200

    Enhance likes plugin state detection & toggle
    Add robust like handling to the likes plugin: attach a click handler on the gallery outer to toggle likes from the toolbar button, compute image IDs from data attributes, dataset or galleryItems, and derive liked state from DOM classes, data-liked attributes or galleryItems.liked/isLiked. Dispatch click events to the matching gallery like icon and schedule follow-up updates to ensure UI consistency. Also set data-image-id on the toolbar button when available. Updated source (src/plugins/likes/lg-likes.ts) and rebuilt distribution files.

--- dist/plugins/likes/lg-likes.es5.js
+++ dist/plugins/likes/lg-likes.es5.js
@@ -67,21 +67,90 @@ var Likes = /** @class */ (function () {
         this.core.LGel.on(lGEvents.updateSlides + ".likes", function () {
             _this.updateLikesState(_this.core.index);
         });
+        this.core.outer.on('click.likes', function (event) {
+            var target = event.target;
+            var likeButton = target === null || target === void 0 ? void 0 : target.closest("." + _this.linkClass);
+            if (!likeButton) {
+                return;
+            }
+            event.preventDefault();
+            _this.toggleLike(_this.core.index);
+        });
+    };
+    Likes.prototype.getImageId = function (index) {
+        var _a, _b, _c, _d, _e;
+        var sourceItem = (_a = this.core.items) === null || _a === void 0 ? void 0 : _a[index];
+        return ((sourceItem === null || sourceItem === void 0 ? void 0 : sourceItem.getAttribute('data-image-id')) || ((_b = sourceItem === null || sourceItem === void 0 ? void 0 : sourceItem.dataset) === null || _b === void 0 ? void 0 : _b.imageId) ||
+            ((_e = (_d = (_c = this.core.galleryItems) === null || _c === void 0 ? void 0 : _c[index]) === null || _d === void 0 ? void 0 : _d.imageId) !== null && _e !== void 0 ? _e : '') + '');
+    };
+    Likes.prototype.getLikedState = function (index) {
+        var _a, _b, _c;
+        var sourceItem = (_a = this.core.items) === null || _a === void 0 ? void 0 : _a[index];
+        var imageId = this.getImageId(index);
+        var galleryItem = (_b = this.core.galleryItems) === null || _b === void 0 ? void 0 : _b[index];
+        if (imageId &&
+            document.querySelector(".sp-gallery-item-" + imageId + ".sp-gallery-item-liked")) {
+            return { imageId: imageId, liked: true, known: true };
+        }
+        var likedAttr = (_c = sourceItem === null || sourceItem === void 0 ? void 0 : sourceItem.getAttribute('data-liked')) !== null && _c !== void 0 ? _c : null;
+        if (likedAttr === '1') {
+            return { imageId: imageId, liked: true, known: true };
+        }
+        if (likedAttr === '0') {
+            return { imageId: imageId, liked: false, known: true };
+        }
+        if (galleryItem &&
+            (galleryItem.liked !== undefined || galleryItem.isLiked !== undefined)) {
+            var likedValue = galleryItem.liked !== undefined
+                ? galleryItem.liked
+                : galleryItem.isLiked;
+            return {
+                imageId: imageId,
+                liked: likedValue === true ||
+                    likedValue === 1 ||
+                    likedValue === '1',
+                known: true,
+            };
+        }
+        return { imageId: imageId, liked: false, known: false };
+    };
+    Likes.prototype.getGalleryLikeIcon = function (imageId) {
+        if (!imageId) {
+            return null;
+        }
+        return document.querySelector(".sp-gallery-like-icon[data-image-id=\"" + imageId + "\"]");
+    };
+    Likes.prototype.toggleLike = function (index) {
+        var _this = this;
+        var imageId = this.getImageId(index);
+        var galleryLikeIcon = this.getGalleryLikeIcon(imageId);
+        if (!galleryLikeIcon) {
+            this.updateLikesState(index);
+            return;
+        }
+        galleryLikeIcon.dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+        }));
+        this.updateLikesState(index);
+        window.setTimeout(function () { return _this.updateLikesState(index); }, 60);
+        window.setTimeout(function () { return _this.updateLikesState(index); }, 180);
+        window.setTimeout(function () { return _this.updateLikesState(index); }, 320);
     };
     Likes.prototype.updateLikesState = function (index) {
-        var _a;
         var $btn = this.core.outer.find("." + this.linkClass).first();
         if (!$btn.get())
             return;
         $btn.removeClass('lg-hide');
-        var sourceItem = (_a = this.core.items) === null || _a === void 0 ? void 0 : _a[index];
-        var imageId = sourceItem === null || sourceItem === void 0 ? void 0 : sourceItem.getAttribute('data-image-id');
-        var isLiked = !!imageId &&
-            !!document.querySelector(".sp-gallery-like-icon[data-image-id=\"" + imageId + "\"].sp-gallery-liked-image");
-        if (isLiked) {
+        var likedState = this.getLikedState(index);
+        if (likedState.imageId) {
+            $btn.attr('data-image-id', likedState.imageId);
+        }
+        if (likedState.liked) {
             $btn.addClass('sp-gallery-liked-image');
         }
-        else {
+        else if (likedState.known) {
             $btn.removeClass('sp-gallery-liked-image');
         }
     };
--- src/plugins/likes/lg-likes.ts
+++ src/plugins/likes/lg-likes.ts
@@ -48,6 +48,114 @@ export default class Likes {
         this.core.LGel.on(`${lGEvents.updateSlides}.likes`, () => {
             this.updateLikesState(this.core.index);
         });
+
+        this.core.outer.on('click.likes', (event: Event) => {
+            const target = event.target as HTMLElement | null;
+            const likeButton = target?.closest(`.${this.linkClass}`);
+
+            if (!likeButton) {
+                return;
+            }
+
+            event.preventDefault();
+            this.toggleLike(this.core.index);
+        });
+    }
+
+    private getImageId(index: number): string {
+        const sourceItem = this.core.items?.[index] as HTMLElement | undefined;
+
+        return (
+            sourceItem?.getAttribute('data-image-id') ||
+            sourceItem?.dataset?.imageId ||
+            ((this.core.galleryItems?.[index] as any)?.imageId ?? '') + ''
+        );
+    }
+
+    private getLikedState(index: number): {
+        imageId: string;
+        liked: boolean;
+        known: boolean;
+    } {
+        const sourceItem = this.core.items?.[index] as HTMLElement | undefined;
+        const imageId = this.getImageId(index);
+        const galleryItem = this.core.galleryItems?.[index] as
+            | { liked?: boolean | number | string; isLiked?: boolean | number | string }
+            | undefined;
+
+        if (
+            imageId &&
+            document.querySelector(
+                `.sp-gallery-item-${imageId}.sp-gallery-item-liked`,
+            )
+        ) {
+            return { imageId, liked: true, known: true };
+        }
+
+        const likedAttr =
+            sourceItem?.getAttribute('data-liked') ?? null;
+
+        if (likedAttr === '1') {
+            return { imageId, liked: true, known: true };
+        }
+
+        if (likedAttr === '0') {
+            return { imageId, liked: false, known: true };
+        }
+
+        if (
+            galleryItem &&
+            (galleryItem.liked !== undefined || galleryItem.isLiked !== undefined)
+        ) {
+            const likedValue =
+                galleryItem.liked !== undefined
+                    ? galleryItem.liked
+                    : galleryItem.isLiked;
+
+            return {
+                imageId,
+                liked:
+                    likedValue === true ||
+                    likedValue === 1 ||
+                    likedValue === '1',
+                known: true,
+            };
+        }
+
+        return { imageId, liked: false, known: false };
+    }
+
+    private getGalleryLikeIcon(imageId: string): HTMLElement | null {
+        if (!imageId) {
+            return null;
+        }
+
+        return document.querySelector(
+            `.sp-gallery-like-icon[data-image-id="${imageId}"]`,
+        );
+    }
+
+    private toggleLike(index: number): void {
+        const imageId = this.getImageId(index);
+        const galleryLikeIcon = this.getGalleryLikeIcon(imageId);
+
+        if (!galleryLikeIcon) {
+            this.updateLikesState(index);
+            return;
+        }
+
+        galleryLikeIcon.dispatchEvent(
+            new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            }),
+        );
+
+        this.updateLikesState(index);
+        window.setTimeout(() => this.updateLikesState(index), 60);
+        window.setTimeout(() => this.updateLikesState(index), 180);
+        window.setTimeout(() => this.updateLikesState(index), 320);
     }
 
     private updateLikesState(index: number): void {
@@ -55,19 +163,15 @@ export default class Likes {
         if (!$btn.get()) return;
 
         $btn.removeClass('lg-hide');
+        const likedState = this.getLikedState(index);
 
-        const sourceItem = this.core.items?.[index] as HTMLElement | undefined;
-        const imageId = sourceItem?.getAttribute('data-image-id');
+        if (likedState.imageId) {
+            $btn.attr('data-image-id', likedState.imageId);
+        }
 
-        const isLiked =
-            !!imageId &&
-            !!document.querySelector(
-                `.sp-gallery-like-icon[data-image-id="${imageId}"].sp-gallery-liked-image`,
-            );
-
-        if (isLiked) {
+        if (likedState.liked) {
             $btn.addClass('sp-gallery-liked-image');
-        } else {
+        } else if (likedState.known) {
             $btn.removeClass('sp-gallery-liked-image');
         }
     }
